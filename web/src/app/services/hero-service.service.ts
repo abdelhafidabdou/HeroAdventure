8b1993ce15db75fa55d4c2c4b8e5553c1fc3d9a1
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-07-28 23:16:47 +0200

    update the service delete the test services

--- web/src/app/services/hero-service.service.ts
+++ web/src/app/services/hero-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
-import {MoveDetails} from "../move-hero/MoveDetails";
+import {MoveDetails} from "../move-hero/move-details";
 import { Observable } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
@@ -15,66 +15,6 @@ export class HeroService {
   constructor(private httpClient:HttpClient) { }
 
 
-
-  getHistory(idAccount,idCustomer){
-
-     const optionRequete = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin':'*',
-        'mon-entete-personnalise':'maValeur'
-      })
-    };
-
-    return this.httpClient
-      .get(this.url+"/accounts/transactions-history?idCustomer="+idCustomer+"&idAcount="+idAccount,optionRequete)
-
-  }
-
-  depositMoney(idAccount,amount){
-    const optionRequete = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin':'*',
-        'mon-entete-personnalise':'maValeur'
-      })
-    };
-    let date: Date = new Date();
-    return this.httpClient.post(this.url+"/accounts/deposit?amount="+amount+"&id="+idAccount,optionRequete)
-
-  }
-
-  withdrawMoney(idAccount,amount){
-    const optionRequete = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin':'*'
-      })
-    };
-    let date: Date = new Date();
-    return this.httpClient.post(this.url+"/accounts/withdraw?amount="+amount+"&id="+idAccount,optionRequete)
-
-  }
-
-  getBalance(idAccount){
-
-    const optionRequete = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin':'*'
-      })
-    };
-    let date: Date = new Date();
-    return this.httpClient.get(this.url+"/accounts/balance/"+idAccount,optionRequete)
-
-  }
-  getHistoryList(idAccount){
-    const optionRequete = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin':'*',
-        'mon-entete-personnalise':'maValeur'
-      })
-    };
-
-    return this.httpClient
-      .get(this.url+"/accounts/"+idAccount+"/history",optionRequete)
-  }
 
 
 
